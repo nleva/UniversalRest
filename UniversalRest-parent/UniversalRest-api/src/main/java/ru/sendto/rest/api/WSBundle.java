338c4a58c1c2c40bd734e3454e19ffbe5c5f870d
nleva <n.lev.a@ya.ru>
2017-05-18 17:44:31 +0300

    SWBundle update

--- UniversalRest-parent/UniversalRest-api/src/main/java/ru/sendto/rest/api/WSBundle.java
+++ UniversalRest-parent/UniversalRest-api/src/main/java/ru/sendto/rest/api/WSBundle.java
@@ -1,21 +1,26 @@
 package ru.sendto.rest.api;
 
 import javax.websocket.CloseReason;
-import javax.websocket.EndpointConfig;
 import javax.websocket.HandshakeResponse;
 import javax.websocket.Session;
 import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
 
 import lombok.Data;
 
 public class WSBundle {
+
+	@Data
+	static class HandshakeBundle {
+		ServerEndpointConfig	config;
+		HandshakeRequest		handshakeRequest;
+		HandshakeResponse		handshakeResponse;
+	}
+
 	@Data
 	static class OnOpen {
-		Session session;
-		EndpointConfig config;
-		HandshakeRequest handshakeRequest;
-		HandshakeResponse handshakeResponse;
-		
+		Session					session;
+		ServerEndpointConfig	config;
 	}
 
 	@Data
@@ -25,13 +30,13 @@ public class WSBundle {
 
 	@Data
 	static class OnClose {
-		Session session;
-		CloseReason closeReason;
+		Session		session;
+		CloseReason	closeReason;
 	}
 
 	@Data
 	static class OnError {
-		Session session;
-		Throwable throwable;
+		Session		session;
+		Throwable	throwable;
 	}
 }
