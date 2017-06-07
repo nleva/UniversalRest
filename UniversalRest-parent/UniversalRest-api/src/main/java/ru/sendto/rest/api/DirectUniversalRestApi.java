2a0804038586f5086e8ea68652c6c9a8d1d373ca
nleva <n.lev.a@ya.ru>
2017-06-07 08:49:06 +0300

    DtoCommons downgrade -> 0.1.3

--- UniversalRest-parent/UniversalRest-api/src/main/java/ru/sendto/rest/api/DirectUniversalRestApi.java
+++ UniversalRest-parent/UniversalRest-api/src/main/java/ru/sendto/rest/api/DirectUniversalRestApi.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
